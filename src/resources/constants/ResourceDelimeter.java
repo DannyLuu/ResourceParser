6ac94b5f34244387859311111d88961895bccc1d
Danny Luu <hisoka365@gmail.com>
2016-06-05 21:35:15 -0400

    Removed files to fix repo.

--- src/resources/constants/ResourceDelimeter.java
+++
@@ -1,9 +0,0 @@
-package resources.constants;
-
-public class ResourceDelimeter {
-	public static final String COMMENT = "#";
-	public static final String ASSIGNMENT = "=";
-	public static final String SPACE = " ";
-	public static final String NEWLINE = "\n";
-	public static final String CARRIAGE_RETURN = "\r";
-}
